04acee85c02c16a484d3867628397f2a3344fd39
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-07 20:12:18 +0100

    Change Email Validation error message

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,7 +1,7 @@
 class Email < ActiveRecord::Base
   attr_accessible :delete_token, :email, :get_token
 
-  validates :email, presence: true, email_format: { message: "Invalid email format" }
+  validates :email, presence: true, email_format: { message: "- invalid format" }
   validates :get_token, presence:true
   validates :delete_token, presence:true
 end
