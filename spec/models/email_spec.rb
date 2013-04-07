ec5b40c44ea950b7f5d8209cb6193014597a8e05
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-07 19:06:47 +0100

    Add email model tests

---
+++ spec/models/email_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe Email do
+
+  before { @email = Email.new(:email => "test@test.com") }
+
+  subject { @email }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:get_token) }
+  it { should respond_to(:delete_token) }
+
+  it { should be_valid }
+
+  describe "when email is not present" do
+    it { should_not be_valid }
+  end
+end
