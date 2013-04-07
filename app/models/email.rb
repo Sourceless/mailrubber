ea669c584f367bae866bb34656889633c984a2fd
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-07 19:35:28 +0100

    Add email validation gem, and validation to Email model.

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,8 @@
 class Email < ActiveRecord::Base
   attr_accessible :delete_token, :email, :get_token
+
+  validates :email, presence: true, 
+                    email_format: { :message => "Invalid email address" } 
+  validates :get_token, presence:true
+  validates :delete_token, presence:true
 end
