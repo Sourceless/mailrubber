7ff3ca5b9b9c5b62b987a9807af8996175d7fe89
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-07 13:22:46 +0100

    Fix tests, move from integration to feature to allow use of capybara.

--- spec/features/emails_spec.rb
+++ spec/features/emails_spec.rb
@@ -4,12 +4,12 @@ describe "Emails" do
 
   describe "Landing Page" do
     it "should have the content 'MailRubber'" do
-      Capybara::visit '/'
+      visit '/'
       page.should have_content('MailRubber')
     end
 
     it "should have a field for email entry" do
-      Capybara::visit '/'
+      visit '/'
       page.should have_content('Email')
     end
   end
