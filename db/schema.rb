7de6c6dec4a368c3d5d5c3ac8c15efcb8d7b9e5e
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-08 14:36:23 +0100

    Set session store to be in-db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406221147) do
+ActiveRecord::Schema.define(:version => 20130408133505) do
 
   create_table "emails", :force => true do |t|
     t.string   "email"
@@ -20,5 +20,15 @@ ActiveRecord::Schema.define(:version => 20130406221147) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
 end
