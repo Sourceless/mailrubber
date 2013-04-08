956f546b999cf6d59067f2165cbe1fd01152181e
Laurence Joseph Smith <laurence@sourceless.org>
2013-04-08 15:49:58 +0100

    Switch all keys over to ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Mailrubber::Application.config.secret_token = '7b80fddf84207a60e440f55345af85bff9263191850883461dc7b216fac60cdd2751b178f1354870b47e876d90f1ce778173f7985adf6b1e165e617c38cb5255'
+Mailrubber::Application.config.secret_token = ENV['SECRET_TOKEN'] 
